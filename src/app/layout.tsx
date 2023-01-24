e3750699f9bdee5d404ab66080f555780608d6e7
grishO92 <grigorgiutta@gmail.com>
2023-01-24 02:23:41 +0100

    added basic navigation no functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import Navigation from './components/Nav'
 import './globals.css'
+
 
 export default function RootLayout({
   children,
@@ -7,12 +9,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+          <Navigation />
+        {children}
+        </body>
     </html>
   )
 }
